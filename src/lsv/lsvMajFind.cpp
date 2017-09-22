b3214eed7da61d7f34d98a74a1b564a622456bb3
flotisable <s09930698@gmail.com>
2017-09-22 16:40:41 +0800

    finish maj find function

--- src/lsv/lsvMajFind.cpp
+++ src/lsv/lsvMajFind.cpp
@@ -28,7 +28,7 @@ ABC_NAMESPACE_IMPL_START
 ///                        DECLARATIONS                              ///
 ////////////////////////////////////////////////////////////////////////
 
-
+bool isMaj( Abc_Obj_t *pObj );
 ////////////////////////////////////////////////////////////////////////
 ///                     FUNCTION DEFINITIONS                         ///
 ////////////////////////////////////////////////////////////////////////
@@ -48,9 +48,28 @@ ABC_NAMESPACE_IMPL_START
 void
 Lsv_NtkMajFind( Abc_Ntk_t * pNtk )
 {
-   // TODO
+  // TODO
+  if( !pNtk ) return; // preconsition
+
+  // variable declaration
+  Abc_Obj_t *pNode;
+  int       i;
+  int       majNum = 0;
+  // end variable declaration
+
+  // find MAJ and print
+  Abc_NtkForEachNode( pNtk, pNode, i )
+  {
+    if( isMaj( pNode ) ) ++majNum;
+  }
+  Abc_Print( ABC_STANDARD, "number of found MAJ : %i\n", majNum );
+  // end find MAJ and print
 }
 
+bool isMaj( Abc_Obj_t *pObj )
+{
+  return true;
+}
 ////////////////////////////////////////////////////////////////////////
 ///                       END OF FILE                                ///
 ////////////////////////////////////////////////////////////////////////
