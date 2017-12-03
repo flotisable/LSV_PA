165fc8b9ccee7e91f2c0e2e6e05f7c0f9ed7f88a
flotisable <s09930698@gmail.com>
2017-12-03 22:42:50 +0800

    modify testcase

--- PA2_testcase/test.v
+++ PA2_testcase/test.v
@@ -1,9 +1,16 @@
-module test( out1, out2, in1, in2, in3, in4 );
+module test( out1, in1, in2, in3, in4, in5, in6, in7, in8 );
 
-  output  out1, out2;
-  input   in1, in2, in3, in4;
+  output  out1;
+  input   in1, in2, in3, in4, in5, in6, in7, in8;
 
-  assign out1 = in1 & in2;
-  assign out2 = in3 & in4;
+  wire and1, and2, and3, and4, and5, and6;
+
+  assign and1 = in1 & in2;
+  assign and2 = in3 & in4;
+  assign and3 = in5 & in6;
+  assign and4 = in7 & in8;
+  assign and5 = and1 & and2;
+  assign and6 = and3 & and4;
+  assign out1 = and5 & and6;
 
 endmodule
