d1004673cc79de355f86d9bf4f3dfb268ac337b7
flotisable <s09930698@gmail.com>
2018-01-13 11:35:40 +0800

    add build s function testing prototype

--- src/lsv/final/Test/LsvMuxDeomp/testLsvMuxDecomp.h
+++ src/lsv/final/Test/LsvMuxDeomp/testLsvMuxDecomp.h
@@ -9,7 +9,9 @@ using std::string;
 bool assert( bool expression, const string &errorTitle, const string &correctResult, const string &programResult );
 
 void testIsMuxDecomp();
+void testBuildS     ();
 
+// abstract model
 struct DdNode
 {
   DdNode *thenNode = nullptr;
@@ -19,8 +21,17 @@ struct DdNode
   DdNode( DdNode *thenNode, DdNode *elseNode ) : thenNode{ thenNode }, elseNode{ elseNode } {}
 };
 
-DdNode *Cudd_T( DdNode *f );
-DdNode *Cudd_E( DdNode *f );
+DdNode*       Cudd_T( DdNode *f );
+DdNode*       Cudd_E( DdNode *f );
+DdNode*       Cudd_Not( DdNode *f );
+int           Cudd_SupportSize( DdManager *dd, DdNode *f );
+unsigned int  Cudd_NodeReadIndex( DdNode *node );
+int           Cudd_ReadPerm( DdManager *dd, int i );
+DdNode*       Cudd_bddIthVar( DdManager *dd, int i );
+DdNode*       Cudd_bddAnd( DdManager *dd, DdNode *f, DdNode *g );
+DdNode*       Cudd_bddXor( DdManager *dd, DdNode *f, DdNode *g );
+DdNode*       Cudd_bddOr( DdManager *dd, DdNode *f, DdNode *g );
+int           Abc_MinInt( int a, int b );
 
 struct Vec_Ptr_t
 {
@@ -33,5 +44,7 @@ size_t      Vec_PtrSize       ( Vec_Ptr_t *pVec               );
 void        Vec_PtrFree       ( Vec_Ptr_t *pVec               );
 
 struct DdManager {};
+
+// end abstract model
 
 #endif
